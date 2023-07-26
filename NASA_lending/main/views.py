2c0c812dabcd26590d1628aa191659461e1fd683
All_C <ilyan64646464@gmail.com>
2023-07-26 22:32:01 +0800

    main application

--- NASA_lending/main/views.py
+++ NASA_lending/main/views.py
@@ -1,3 +1,16 @@
+from django.views.defaults import page_not_found
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# обработка единственной страницы сайта
+def index(request):
+    return render(request, 'main/nasa.html')
+
+
+# обработка отсутствующей страницы
+def page_not_found_view(request, exception):
+    response = page_not_found(request, exception,
+                              template_name='main/404.html')
+    response.status_code = 404
+    return response
